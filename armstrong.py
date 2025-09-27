b4cbbd01f085bbeb1b63bd8cabc4b9c3ce642425
rmreshma72 <100831815+rmreshma72@users.noreply.github.com>
2025-09-27 22:42:43 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+def armstrong(num):
+    result=0
+    l = len(str(num))
+    while (num>0):
+        rem=num%10
+        result = result + rem**l
+        num=num//10
+    return result
+
+num=int(input("Enter a number:"))
+if (armstrong(num)==num):
+    print(num, "is an Armstrong number")
+else:
+    print(num,"is not an Armstrong number")
